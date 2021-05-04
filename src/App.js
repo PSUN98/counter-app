869fb3187338c3aec8fc4e6fd87f584409803243
PSUN98 <psyq98@gmail.com>
2021-05-04 16:11:59 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from me
+          Learn React from Priscila
         </a>
       </header>
     </div>
